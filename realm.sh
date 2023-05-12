9d851837cebd9a17e5bec58ffd88d7836834ba07
vpsxb <hhddx0@outlook.com>
2023-05-12 23:46:00 +0800

    fix

--- realm.sh
+++ realm.sh
@@ -244,7 +244,7 @@ echo -e "
  ${Green_font_prefix}3.${Font_color_suffix} 启动 RealM
  ${Green_font_prefix}4.${Font_color_suffix} 停止 RealM
  ${Green_font_prefix}5.${Font_color_suffix} 重启 RealM"
-——————————————
+——————————————"
 check_status
 
 read -p " 请输入数字后[0-11] 按回车键:
